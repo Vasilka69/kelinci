8ba67a4c4e8c7be7444433c07163b45e487c10df
Vasilka69 <114226684+Vasilka69@users.noreply.github.com>
2024-02-15 16:22:47 +0500

    Update XLS.java

--- examples/xls/src/XLS.java
+++ examples/xls/src/XLS.java
@@ -1,19 +1,16 @@
 package src;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class XLS
 {
-    public static void main(final String[] args) throws IOException, InvalidFormatException {
+    public static void main(final String[] args) {
         System.err.println("Current args:" + Arrays.toString(args));
 
-        File file = new File(args[0]);
         try {
+            File file = new File(args[0]);
             IndividualPersonImportServiceImpl individualPersonImportService = new IndividualPersonImportServiceImpl();
             List<IndividualPersonImport> persons = individualPersonImportService.parseUsersFromExcel(file);
 
